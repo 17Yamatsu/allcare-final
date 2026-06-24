61b47f0929bd93866920ac88c2001721591b305c
kayron-wolff <gustavo.asus3@outlook.com>
2026-06-24 20:21:19 -0300

    Alterações para funcionar no Expo Go

--- AllCare-app/Front-end/src/services/api.js
+++ AllCare-app/Front-end/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8001";
+const API_URL = "http://{API_HOST_IP}:8001";
 
 async function request(path, options = {}) {
   const response = await fetch(`${API_URL}${path}`, {
